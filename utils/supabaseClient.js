b937b8da12622480cdcc0ce5d56f0315eb3f98db
SL <signaturelabel.brand@gmail.com>
2025-02-20 12:39:46 +0900

    new fix w grk

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -5,7 +5,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('supabaseUrl and supabaseKey are required.');
+  throw new Error('Missing Supabase URL or Anon Key in environment variables.');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
